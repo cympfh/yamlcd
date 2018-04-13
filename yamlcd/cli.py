e0eb5e7a299556fc7ccbf2073837131ba64b94f7
cympfh <cympfh@gmail.com>
2018-04-13 17:39:05 +0900

    error

--- yamlcd/cli.py
+++ yamlcd/cli.py
@@ -19,14 +19,21 @@ def insert(data, memo, path, filename):
 def main(dir):
 
     memo = defaultdict(list)  # ['x.y.z'] = "hoge.yml"
+    err = False
 
     for f in glob(f"{dir}/**/*.yml", recursive=True):
-        data = yaml.load(open(f))
-        insert(data, memo, [], f)
+        try:
+            data = yaml.load(open(f))
+            insert(data, memo, [], f)
+        except yaml.YAMLError:
+            click.echo(f"Warn: ParseError in {f}", err=True)
 
     for key in memo:
         if len(memo[key]) > 1:
-            print(f"Error: `{key}` appeared in: {', '.join(memo[key])}")
+            click.echo(f"Error: `{key}` appeared in: {', '.join(memo[key])}", err=True)
+            err = True
+
+    exit(err)
 
 
 if __name__ == '__main__':
